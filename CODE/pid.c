558d3a238a3ba9d01117b6a14fa1581024186c78
SORmaker <1583211717@qq.com>
2022-11-16 16:17:36 +0800

    整理了一下pid

--- CODE/pid.c
+++ CODE/pid.c
@@ -18,45 +18,44 @@ int16 PID_speed(int16 setspeed, int16 dat, float *PID)
     int16 nowspeed;
     float m;
     int16 output;
-    uint8 flag;
-    nowspeed = dat;
+    nowspeed = dat;//当前速度
 
-    error1 = setspeed - nowspeed;
+    error1 = setspeed - nowspeed;//误差
 
+    //积分分离
     if(error1 >= integral_partitioning_limit || error1 <= -integral_partitioning_limit)
     {
-        flag = 0;
-        sum = 0;
+        sum = 0;//清零积分项，防止出现下次开始积分时，还保留上次的积分项
     }
-    else flag = 1;
-//    flag = 1;
+    else
+    {
+        sum += error1;
+    }
 
-    sum += flag*error1;
-
+    //积分限幅
     if(sum >= integral_limit) sum = integral_limit;
     if(sum <= -integral_limit) sum = -integral_limit;
 
-    m = sum*PID[KI];
-
-    if(error1 >= 600) error1 = 600;
-    if(error1 <= -600) error1 = -600;
+    m = sum*PID[KI];//单独拿出来没啥特别含义，单纯为了看i效果
 
     output = (int16)(PID[KP]*error1 + m + PID[KD]*(error1 - error0));
 
-    error0 = error1;
+    error0 = error1;//误差更新
 
+    //输出限幅
     if(output >= output_limit) output = output_limit;
     if(output <= -output_limit) output = -output_limit;
 
     return output;
 }
 
+//绝对值函数 用于编码器积路程
 int16 my_abs(int16 value)
 {
     return value > 0 ? value : -value;
 }
 
-//编码器读值
+//编码器积路程
 uint32 Read_speed(void)
 {
     uint32 dis;
@@ -109,10 +108,10 @@ int16 PID_steer(int16 error_now, float *PID)//[-752,752]
 //    float kd = 0.18;//!!!!修改后无负号
 
     outdata = (int16)(PID[KP]*error_now + PID[KD]*(error_last-error_now));
-    outdata = middata + outdata;
+    outdata = middata + outdata;//中值加pid数值
 
-    if(outdata >= 850) outdata = 850;//左750
-    if(outdata <= 700) outdata = 700;//右615
+    if(outdata >= 850) outdata = 850;//左850
+    if(outdata <= 700) outdata = 700;//右700
 
     error_last = error_now;
 
--- CODE/pid.h
+++ CODE/pid.h
@@ -19,6 +19,6 @@ int16 PID_speed(int16 setspeed, int16 dat, float *PID);
 uint32 Read_speed(void);
 void Direct(int16 left,int16 right);
 int16 PID_steer(int16 error, float *PID);
-
+int16 my_abs(int16 value);
 
 #endif /* CODE_PID_H_ */
